742134c3c2ec5c377afed183199eec8c358f61b6
kl-twizzy <kirill89090453074@yandex.ru>
2024-11-28 23:41:10 +0300

    kt3

--- go.mod
+++ go.mod
@@ -1,10 +1,9 @@
-module GO3KT
+module myProject
 
 go 1.23.0
 
-require github.com/gen2brain/beeep v0.0.0-20240516210008-9c006672e7f4
-
 require (
+	github.com/gen2brain/beeep v0.0.0-20240516210008-9c006672e7f4 // indirect
 	github.com/go-toast/toast v0.0.0-20190211030409-01e6764cf0a4 // indirect
 	github.com/godbus/dbus/v5 v5.1.0 // indirect
 	github.com/nu7hatch/gouuid v0.0.0-20131221200532-179d4d0c4d8d // indirect
--- kt3.go
+++ kt3.go
@@ -1,69 +1,93 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strconv"
+	"strings"
 	"sync"
 	"time"
 
 	"github.com/gen2brain/beeep"
 )
 
-type Lion struct {
-	HighSpeed         int
+// Структура для животного
+type Animal struct {
+	Name              string
+	HighSpeed         bool
+	Size              string
+	ClimbTree         bool
 	RecognizeDiseases bool
 }
 
-type Giraffe struct {
-	Size      int
-	ClimbTree bool
+// Функция для загрузки информации о животном
+func loadAnimalInfo(animal Animal) {
+	// Имитация загрузки информации
+	time.Sleep(time.Second)
+	fmt.Printf("Загружена информация о %s:\n", animal.Name)
+	fmt.Printf("HighSpeed: %v\n", animal.HighSpeed)
+	fmt.Printf("Size: %s\n", animal.Size)
+	fmt.Printf("ClimbTree: %v\n", animal.ClimbTree)
+	fmt.Printf("RecognizeDiseases: %v\n", animal.RecognizeDiseases)
 }
 
-type Snake struct {
-	Length   int
-	Venomous bool
-}
-
-func processLion(wg *sync.WaitGroup) {
-	defer wg.Done()
-	lion := Lion{HighSpeed: 80, RecognizeDiseases: true}
-	fmt.Printf("Лев - Скорость: %d км/ч, Распознавание болезней: %v\n", lion.HighSpeed, lion.RecognizeDiseases)
-
-	go func() {
-		beeep.Notify("Лев", "Обработана информация о льве", "")
-	}()
-	time.Sleep(1 * time.Second)
-}
-
-func processGiraffe(wg *sync.WaitGroup) {
-	defer wg.Done()
-	giraffe := Giraffe{Size: 500, ClimbTree: false}
-	fmt.Printf("Жираф - Размер: %d кг, Может залезать на деревья: %v\n", giraffe.Size, giraffe.ClimbTree)
-
-	go func() {
-		beeep.Notify("Жираф", "Обработана информация о жирафе", "")
-	}()
-	time.Sleep(1 * time.Second)
-}
-
-func processSnake(wg *sync.WaitGroup) {
-	defer wg.Done()
-	snake := Snake{Length: 2, Venomous: true}
-	fmt.Printf("Змея - Длина: %d м, Ядовитая: %v\n", snake.Length, snake.Venomous)
-
-	go func() {
-		beeep.Notify("Змея", "Обработана информация о змее", "")
-	}()
-	time.Sleep(1 * time.Second)
+// Функция для отправки уведомления
+func sendNotification(animal Animal) {
+	err := beeep.Notify("Информация о животном", animal.Name, "")
+	if err != nil {
+		panic(err)
+	}
 }
 
 func main() {
+	// Создаем WaitGroup для отслеживания горутин
 	var wg sync.WaitGroup
 
-	wg.Add(3)
-	go processLion(&wg)
-	go processGiraffe(&wg)
-	go processSnake(&wg)
+	// Список животных
+	animals := []Animal{
+		{Name: "Лев", HighSpeed: true, Size: "Большой", ClimbTree: false, RecognizeDiseases: false},
+		{Name: "Обезьяна", HighSpeed: false, Size: "Средний", ClimbTree: true, RecognizeDiseases: true},
+		{Name: "Зебра", HighSpeed: true, Size: "Средний", ClimbTree: false, RecognizeDiseases: false},
+		{Name: "Тигр", HighSpeed: true, Size: "Большой", ClimbTree: false, RecognizeDiseases: false},
+		{Name: "Кот", HighSpeed: false, Size: "Маленький", ClimbTree: true, RecognizeDiseases: false},
+	}
 
+	// Выводим список животных для выбора
+	fmt.Println("Доступные животные:")
+	for i, animal := range animals {
+		fmt.Printf("%d. %s\n", i+1, animal.Name)
+	}
+
+	// Считываем выбор пользователя
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Выберите животных (через запятую): ")
+	input, _ := reader.ReadString('\n')
+	input = strings.TrimSpace(input)
+	choices := strings.Split(input, ",")
+
+	// Запускаем горутины для выбранных животных
+	for _, choice := range choices {
+		index, err := strconv.Atoi(strings.TrimSpace(choice))
+		if err != nil || index < 1 || index > len(animals) {
+			fmt.Printf("Неверный выбор: %s\n", choice)
+			continue
+		}
+
+		animal := animals[index-1]
+		wg.Add(1)
+		go func(a Animal) {
+			defer wg.Done()
+			loadAnimalInfo(a)
+			go sendNotification(a) // Асинхронный вызов уведомления
+		}(animal)
+	}
+
+	// Ожидаем завершения всех горутин
 	wg.Wait()
-	fmt.Println("Все животные обработаны.")
+
+	// Ожидание завершения всех звуков (в данном случае, уведомлений)
+	time.Sleep(2 * time.Second)
+
+	fmt.Println("Программа завершена.")
 }
